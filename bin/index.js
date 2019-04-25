3f47a684e9c0e21aaaa5b1d378c24c4b845e7d05
stevekeol <stevekeol@users.noreply.github.com>
2019-04-25 11:37:30 +0800

    测试__dirname和processs.cwd()的区别

--- bin/index.js
+++ bin/index.js
@@ -20,14 +20,11 @@
   );
 
   program
-  .name('rrg')
   .version('1.1.0', '-v, --version') //会将-v和--version添加到命令行中，调用时可通过带上该参数获取该脚手架的版本号（命令 -v/--version）
   .command('init <name>') //定义初始化命令,name参数必传，作为项目文件名
   .action(name => { //action是执行command时的回调，项目生成过程发生在该回调中
-    console.log(name);
-
     if (!fs.existsSync(name)) {
-      console.log('正在创建项目...');
+      console.log(`正在创建项目(${name})...`);
       inquirer.prompt([ //参数为对象数组，用于在命令行逐条提示用户输入
         {
             name: 'description',
@@ -39,6 +36,8 @@
         }
       ]).then(answers => { //answers为用户输入参数组成的对象
         console.log(answers);
+        console.log(__dirname);
+        console.log(process.cwd());
         const spinner = ora('正在下载模板...\n');
         spinner.start();
         child_process.exec('git clone https://github.com/stevekeol/react-redux-generator-template', function(err, stdout, stderr) { //运行命令进行下载模块的方式3;参考文末
@@ -47,7 +46,7 @@
             console.log(symbols.error, chalk.red('模板下载失败'))
             console.log(err);
           } else {
-            spinner.succeed();
+            spinner.succeed
 
             //更改项目文件名
             shell.mv(__dirname + '/react-redux-generator-template', __dirname + '/' + name); //将在当前目录刚下载成功的模板，重命名为用户输入的项目名
